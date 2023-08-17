01aed84b0e4d4059b55852266e0c14391ea5a18c
OksanaMosk <130758466+OksanaMosk@users.noreply.github.com>
2023-08-17 17:01:48 +0300

    All

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,33 +5,39 @@ const renderList = (arr) =>
     arr 
         .map(
             (item) =>
-           `<li data-id="${item.id}" class="item">
-                    <a class="gallery__link" href="large-image.jpg">
-                        <img
-                            class="gallery__image"
-                            src="small-image.jpg width="300" height="200"
-                            data-source="large-image.jpg"
-                            alt="Image description"
-                        />
-                    </a>
-                </li>`
+           `<li data-id ="id" class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image lazyload"
+   src="${item.preview}"
+    data-source="large-image.jpg"
+      alt="${item.description}"
+      width="200" height="150"
+    />
+  </a>
+</li>`
         )
         .join("");
+
 
 const handleListClick = (event) => {
     if (event.currentTarget === event.target) {
         return;
     }
 
+    const currentListItem = event.target.closest('.gallery__item');
+    const itemId = currentListItem.dataset.id;
+    const everyGalleryItems = galleryItems.find((item) => item.id === +itemId);
+    const modalInstance = basicLightbox.create(`<div class="modal">
+    <img src="${galleryItems.original}" 
+    width="1000" height="750"/>
+    </div>`)
 
-const instance = basicLightbox.create(`
-    <img src="./gallery-items.js" width="800" height="600">
-`)
-
-instance.show()
-
+modalInstance.show()
         }
-listEl.insertAdjacentHTML('beforeend', renderList(galleryItems));
-listEl.addEventListener("click", handleListClick)
-console.log(galleryItems);
+listEl.insertAdjacentHTML("beforeend", renderList(galleryItems));
+
+listEl.addEventListener("click", handleListClick);
+
+
 
