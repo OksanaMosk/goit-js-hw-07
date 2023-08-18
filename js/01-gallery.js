4fc2d3b546570c6b37ce31998d574e3de3d15ddb
OksanaMosk <130758466+OksanaMosk@users.noreply.github.com>
2023-08-18 16:59:00 +0300

    All

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,55 +1,51 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const listEl = document.querySelector(".gallery");
-const renderList = (arr, container) => {
-  const markup = arr
-    .map(
-      (item) =>
-        `<li class="gallery__item">
-  <a class="gallery__link" href="${item.original}">
+const everyEl = renderList(galleryItems);
+listEl.insertAdjacentHTML("beforeend", everyEl);
+
+
+function renderList (items) {
+    return items.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
     <img
-      class="gallery__image lazyload"
-   src="${item.preview}"
-    data-source="large-image.jpg"
-      alt="${item.description}"
-      width="200" height="150"
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+      width="300" height="225"
     />
   </a>
 </li>`
-    )
-    .join("");
-
-  container.insertAdjacentHTML("beforeend", markup);
+    })
+ .join("")
 };
+listEl.addEventListener("click", handleListClick);
 function handleListClick(event) {
   event.preventDefault();
-  const imgGal = event.target;
-
-  if (imgGal.nodeName !== "IMG") {
+  const imgGallery = event.target;
+  if (imgGallery.nodeName !== "IMG") {
     return;
   }
-  const originalImg = imgGal.parentNode.href;
-
+  const originalImg = imgGallery.parentNode.href;
   const modalInstance = basicLightbox.create(
     `<img src="${originalImg}" width="800" height="600">`,
     {
-      onShow: (modalinstance) => {
-        window.addEventListener("keydown", keyPress);
+      show: (modalinstance) => {
+        window.addEventListener("keydown", handler);
       },
-      onClose: (modalinstance) => {
-        window.removeEventListener("keydown", keyPress);
+      close: (modalinstance) => {
+        window.removeEventListener("keydown", handler);
       },
     }
   );
   modalInstance.show();
 }
-
-function keyPress(e) {
+function handler(e) {
   if (e.code === "Escape") {
     return;
   }
   modalInstance.close();
 }
 
-renderList(galleryItems, listEl);
-listEl.addEventListener("click", handleListClick);
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const listEl = document.querySelector(".gallery");
+const everyEl = renderList(galleryItems);
+listEl.insertAdjacentHTML("beforeend", everyEl);
+function renderList (items) {
+    return items.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+   src="${preview}"
+      alt="${description}"
+      width="200" height="150"
+    />
+  </a>
+</li>`
+    })
+      
+      .join("");
+  
+};
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+ 
+});
+
 
 console.log(galleryItems);
